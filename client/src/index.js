1b61896267640959009f7fb2ccf65e5a16c7586e
samuel abegunde <abegundeosamuel@gmail.com>
2021-09-14 06:49:08 +0100

    added changes

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { createStore, applyMiddleware } from 'redux'
@@ -23,20 +22,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 );
-=======
-import './index.css';
-import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
->>>>>>> 66c00ee90a76a95026c064a4df4298f692f94ffe

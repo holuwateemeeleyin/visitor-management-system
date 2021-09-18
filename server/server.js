b358485d7110d45b504e70ca9c759ce685b6d05f
samuel abegunde <abegundeosamuel@gmail.com>
2021-09-18 09:01:20 +0100

    added connection to Atlas

--- server/server.js
+++ server/server.js
@@ -1,26 +1,57 @@
+const path = require('path')
+const dotenv =require ('dotenv')
+dotenv.config({path: path.resolve(__dirname, './.env')})
 const express = require('express');
-const bodyParser = require('body-parser')
-const cookieParser = require ('cookie-parser')
+const cookieParser = require('cookie-parser')
 const mongoose = require('mongoose')
-const config = require('./config/config').get(process.env.NODE_ENV)
 const app = express()
+app.use(express.json())
+
 
 // Seting database
 mongoose.Promise = global.Promise
-mongoose.connect(config.DATABASE)
+
+mongoose.connect (
+    process.env.MONGODB_STRING,
+    {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+    }
+)
+.then(() => console.log("MongoDB has been connected"))
+.catch((err) => console.log(err));
 
 
-const visitor = require ('./routes/visitor')
-const admin = require ('./routes/admin')
+const visitor = require('./routes/visitor')
+const admin = require('./routes/admin')
 
 
-app.use(bodyParser.json());
 app.use(cookieParser())
 
 app.use('/api/visitor', visitor)
 app.use('/api/admin', admin)
 
-const port = process.env.PORT || 3001;
-app.listen(port, ()=> {
-    console.log('Server is running now');
-})
+
+
+
+
+// const port = process.env.PORT || 3001;
+// app.listen(port, ()=> {
+//     console.log('Server is running now');
+// })
+
+
+// Serve static assets in production
+// if (process.env.NODE_ENV === 'production') {
+//     // Set static folder
+//     app.use(express.static('client/build'));
+
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//     });
+// }
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

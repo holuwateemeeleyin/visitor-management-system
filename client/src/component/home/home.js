7f232744234fd392a396a25803f240ada9e08592
samuel abegunde <abegundeosamuel@gmail.com>
2021-09-18 08:53:41 +0100

    added react reaveal animation

--- client/src/component/home/home.js
+++ client/src/component/home/home.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Slide from 'react-reveal/Slide';
+import Bounce from 'react-reveal/Bounce'
+import Zoom from 'react-reveal/Zoom'
+import Flip from 'react-reveal/Flip'
+
 
 const Home = () => {
 
@@ -8,25 +13,32 @@ const Home = () => {
             {/* <div className="container"> */}
             <div className="container-row">
                 <div className="col-3 welcome">
-                    <h1>
-                        Welcome to <br />
-                        Code Squad <br />
-                        Visitor Management <br />
-                        System
-                    </h1>
-                    <p style={{color:'#1a1f36'}}> Our guests are important to us. Hence, this is a way to save the stress of doing it traditional way. Enjoy your stay.
-                    </p>
-                    <p><strong>Note:</strong> Your data are saved</p>
-                    <br/>
-                    <Link to="/checkin" className="checkin">
-                        Check In
-                    </Link>
-                </div>
-                <div className="col-6">
-                    {/* <img src='../images/visitor-management-system.png' alt='image1' height="450px" width="450px"/> */}
-                    <img src='../images/image2.png' alt='image1' height="450px" width="450px"/>
-                </div>
 
+                    <Slide left>
+                        <h1>
+                            Welcome to <br/>
+                            <i>Ems'</i> Visitor <br/>
+                            Management System
+                        </h1>
+                    </Slide>
+                    <Zoom right cascade>
+                        <p style={{ color: '#1a1f36'}}> Our guests are important to us. We provide a way to save the stress of doing it traditional way. Enjoy your stay.
+                        </p>
+                        <p><strong>Note:</strong> Your data are saved</p>
+                        <br />
+                    </Zoom>
+                    <Bounce>
+                        <Link to="/checkin" className="checkin">
+                            Check In
+                        </Link>
+                    </Bounce>
+
+                </div>
+                <Flip>
+                    <div className="col-6">
+                        <img src='../images/image2.png' alt='image1' height="450px" width="450px" />
+                    </div>
+                </Flip>
             </div>
         </div>
     )

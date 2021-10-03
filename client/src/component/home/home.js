a7439a7ec1ee25b60cc97ea2534781acb16de4e8
samuel abegunde <abegundeosamuel@gmail.com>
2021-10-03 21:13:37 +0100

    added changes

--- client/src/component/home/home.js
+++ client/src/component/home/home.js
@@ -21,12 +21,15 @@ const Home = () => {
                             Management System
                         </h1>
                     </Slide>
-                    <Zoom right cascade>
-                        <p style={{ color: '#1a1f36'}}> Our guests are important to us. We provide a way to save the stress of doing it traditional way. Enjoy your stay.
+                    <Zoom left cascade>
+                        <div>
+                        <p style={{ color: '#1a1f36', fontSize:'20px'}} classname="guests"> Our guests are important to us. We provide a way to save the stress of doing it traditional way. 
+                            Enjoy your stay.
                         </p>
-                        <p><strong>Note:</strong> Your data are saved</p>
-                        <br />
+                        <p><strong>Note:</strong> Your data is saved</p>
+                        </div>
                     </Zoom>
+                    <br/>
                     <Bounce>
                         <Link to="/checkin" className="checkin">
                             Check In
@@ -36,7 +39,7 @@ const Home = () => {
                 </div>
                 <Flip>
                     <div className="col-6">
-                        <img src='../images/image2.png' alt='image1' height="450px" width="450px" />
+                        <img src='../images/image2.png' alt='image1' className='responsive' />
                     </div>
                 </Flip>
             </div>

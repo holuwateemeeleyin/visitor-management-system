f6ef1a5a3db02e3fd801071b2a5503560d17afc1
unknown <abegundeosamuel@gmail.com>
2022-02-19 14:29:07 +0100

    make changes to the header responsiveness

--- client/src/hoc/layout.js
+++ client/src/hoc/layout.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import Header from '../component/header'
+import HeaderMobile from '../component/header/headerMobile'
+import Header from '../component/header/header'
 const Layout =(props)=> {
     return (
         <div>
+            <HeaderMobile/>
             <Header/>
             <div>
                 {props.children}

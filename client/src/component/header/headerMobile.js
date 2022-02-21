1afc78218289100c2e4c8c808c141fa329732f45
unknown <abegundeosamuel@gmail.com>
2022-02-21 09:21:35 +0100

    added logo to header

---
+++ client/src/component/header/headerMobile.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import Nav from './sidenav/sidenav'
+import './headerMobile.css'
+import logo from '../Assets/emLogo.png'
+
+class Header extends Component {
+    state = {
+        showNav: false
+    }
+    onHideNav = () => {
+        this.setState({ showNav: false })
+    }
+    render() {
+        return (
+            <header>
+                <div className="open_nav">
+                    <FontAwesomeIcon
+                        onClick={() => this.setState({ showNav: true })}
+                        // className="bars "
+                        icon={faBars}
+                        style={{
+                            color: '#008116',
+                            fontSize:'2em',
+                            cursor: 'pointer'
+                        }}
+                    />
+                </div>
+                <Nav
+                    showNav={this.state.showNav}
+                    onHideNav={()=>this.onHideNav()}
+                />
+                <Link to="/" className="logo">
+                    {/* Ems' Place */}
+                    <img src={logo} alt='logo' width='70px'/>
+                </Link>
+            </header>
+        )
+    }
+}
+
+export default Header

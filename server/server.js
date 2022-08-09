68651cbc568cdf24448d685127e15344b2958a36
unknown <abegundeosamuel@gmail.com>
2022-08-09 19:32:39 +0100

    Fixed heroku error

--- server/server.js
+++ server/server.js
@@ -36,50 +36,27 @@ app.use('/api/admin', admin)
 
 
 
-
-
-// const port = process.env.PORT || 3001;
-// app.listen(port, ()=> {
-//     console.log('Server is running now');
-// })
-
-
-// Serve static assets in production
 // if (process.env.NODE_ENV === 'production') {
 //     // Set static folder
-//     app.use(express.static('client/build'));
+//     app.use(express.static(path.join(__dirname, "../client/build")))
 
 //     app.get('*', (req, res) => {
-//         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 //     });
 // }
 
-
-// ... other app.use middleware 
-// app.use(express.static(path.join(__dirname, "client", "build")))
-
-// // Right before your app.listen(), add this:
-// app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-// });
-
-
-
-
-// Right before your app.listen(), add this:
-// app.get("/*", (req, res) => {
-//     const path = require('path');
-//     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
-// });
-
 if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static(path.join(__dirname, "../client/build")))
-
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-    });
-}
+    app.use(express.static(path.join(__dirname, '../client/build')))
+  
+    app.get('*', (req, res) =>
+      res.sendFile(
+        path.resolve(__dirname, '../', 'client', 'build', 'index.html')
+      )
+    )
+  } else {
+    app.get('/', (req, res) => res.send('Please set to production'))
+  }
+  
 
 const PORT = process.env.PORT || 3001;
 

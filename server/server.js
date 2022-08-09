afdff7c562d39e6f8fa0a1299909bac7c8af29fa
unknown <abegundeosamuel@gmail.com>
2022-08-09 19:44:13 +0100

    revert back

--- server/server.js
+++ server/server.js
@@ -36,27 +36,14 @@ app.use('/api/admin', admin)
 
 
 
-// if (process.env.NODE_ENV === 'production') {
-//     // Set static folder
-//     app.use(express.static(path.join(__dirname, "../client/build")))
-
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-//     });
-// }
-
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')))
-  
-    app.get('*', (req, res) =>
-      res.sendFile(
-        path.resolve(__dirname, '../', 'client', 'build', 'index.html')
-      )
-    )
-  } else {
-    app.get('/', (req, res) => res.send('Please set to production'))
-  }
-  
+    // Set static folder
+    app.use(express.static(path.join(__dirname, "../client/build")))
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 3001;
 
